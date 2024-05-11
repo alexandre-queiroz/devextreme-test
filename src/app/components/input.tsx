5630ef149df94592e23617860016c1b50345948b
Alexandre Queiroz <aaugustodeoliveiraqueiroz@gmail.com>
2024-05-11 00:24:30 -0300

    feat: login screen

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,11 +1,20 @@
-export default function LoginInput() {
+import { LucideProps } from "lucide-react"
+import { ForwardRefExoticComponent, RefAttributes } from "react"
+
+interface InputProsp {
+    type?: string,
+    placeholder: string,
+    Source: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>
+}
+
+export default function Input({ type = "text", placeholder, Source }: InputProsp) {
 
     return (
-        <view className='border-2 mt-6 rounded-lg p-1 shadow-sm'>
-            <text className='md:text-[10px] text-start text-[#8A8A8A] font-bold'>Usuário </text>
-            <view className="flex flex-row">
-                <text className="text-[#8A8A8A]">img</text>
-            <input className='w-full p-3 md:p-0' type='text' />
+        <view className='border-2 mt-6 rounded-lg py-1 px-2 shadow-sm'>
+            <text className='text-[14px] text-start text-[#8A8A8A] font-bold'>{placeholder}</text>
+            <view className="flex flex-row gap-1 items-center">
+                <Source color="black" size={24} strokeWidth={1.5} />
+                <input className='text-black w-full p-0' type={type} />
             </view>
         </view>
     )
